fc72a7b2410c221edd263dfe143dddfefa373645
Andres Ramirez <aramirez9933@gmail.com>
2026-04-28 18:41:57 -0400

    Fixed code logic for categoryController

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -18,7 +18,8 @@ export async function getCategoryByIdHandler(req,res,next){
 
 export async function createCategoryHandler(req,res,next){
     try{
-        const category = await service.createCategory(req.body);
+        const categoryData = { ...req.body, userId: req.user.id};
+        const category = await service.createCategory(categoryData);
         res.status(201).json(category);
     }catch(err){
         next(err);
@@ -27,15 +28,50 @@ export async function createCategoryHandler(req,res,next){
 
 export async function updateCategoryHandler(req,res,next){
     try{
-        const category = await service.updateCategory(req.params.id,req.body);
-        res.status(200).json(category);
-    }catch(err){next(err);}
+        const id = parseInt(req.params.id);
+        const category = await service.getCategoryById(id);
+
+    
+
+    if(!category){
+        const err = new Error('Category not found');
+        err.status = 404;
+        throw err;
+    }
+
+    if(category.userId !== req.user.id){
+        const err = new Error('Forbidden: You do not own this category');
+        err.status=403;
+        throw err;
+    }
+
+    const updated = await service.updateCategory(id,req.body);
+    res.status(200).json(updated);
+}catch(err){next(err);}
+
 }
 
 export async function deleteCategoryHandler(req,res,next){
     try{
-        await service.deleteCategory(req.params.id);
-        res.status(204).send();
+        const id = parseInt(req.params.id);
+        
+        const category = await service.getCategoryById(id);
 
-    }catch(err){next(err);}
+        if(!category){
+            const err = new Error('Category not found');
+            err.status=404;
+            throw err;
+        }
+
+        if(category.userId !== req.user.id){
+            const err = new Error('Forbidden: You do not own this category');
+            err.status =403;
+            throw err;
+        }
+
+        await service.deleteCategory(id);
+        res.status(204).send();
+    }catch(err){
+        next(err);
+    }
 }
